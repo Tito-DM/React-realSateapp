15bd7582e68e1e31c4266f2b3229520b374a48e9
pyong <muandatito@gmail.com>
2020-12-26 21:19:01 +0000

    complete page 1

--- src/App.js
+++ src/App.js
@@ -1,24 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
-
+import React from "react";
+import Navbar from "../src/Components/navbar";
+import GlobalStyle from "./globalStyled";
+import Hero from "../src/Components/hero";
+import { SliderData } from "./data/SliderData";
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <GlobalStyle />
+      <Navbar />
+      <Hero slides ={SliderData} />
+    </>
   );
 }
 
---
+++ src/data/SliderData.js
@@ -0,0 +1,39 @@
+import ImageOne from "../images/house-1.jpg"
+import ImageTwo from "../images/house-2.jpg"
+import ImageThree from "../images/house-3.jpg"
+import ImageFour from "../images/house-4.jpg"
+
+export const SliderData = [
+  {
+    title: "Luxury villa in Bali, Indonesia",
+    price: "$4,2800,00",
+    path: "/home",
+    label: "view Home",
+    image: ImageOne,
+    alt: "House",
+  },
+  {
+    title: "Luxury villa in Bali, Indonesia",
+    price: "$4,2800,00",
+    path: "/home",
+    label: "view Home",
+    image: ImageTwo,
+    alt: "House",
+  },
+  {
+    title: "Luxury villa in Bali, Indonesia",
+    price: "$4,2800,00",
+    path: "/home",
+    label: "view Home",
+    image: ImageThree,
+    alt: "House",
+  },
+  {
+    title: "Luxury villa in Bali, Indonesia",
+    price: "$4,2800,00",
+    path: "/home",
+    label: "view Home",
+    image: ImageFour,
+    alt: "House",
+  },
+];
